44a1e70de9b9250adee6f57fbd381d263f3c0462
Dante <dante.giuliano@est.fi.uncoma.edu.ar>
2024-02-10 11:08:13 -0300

    update deploy

---
+++ dist/.nitro/prerender/chunks/hello.mjs
@@ -0,0 +1,6 @@
+import { defineEventHandler } from 'file://C:/Users/dante/Desktop/analog/blog/node_modules/h3/dist/index.mjs';
+
+const hello = defineEventHandler(() => ({ message: "Hello World" }));
+
+export { hello as default };
+//# sourceMappingURL=hello.mjs.map
